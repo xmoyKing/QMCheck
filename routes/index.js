f55855fc49c945de5f8597a9275c184ff6399d3e
xmoyking <xmoyking@gmail.com>
2017-03-02 23:31:34 +0800

    修改 index页面

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: '青梅小组查卡页面' });
 });
 
 module.exports = router;
