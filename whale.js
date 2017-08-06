af882d973e12ad9b71dd2fe29c750585b6810cd0
xmoyking <xmoyking@gmail.com>
2017-08-06 19:04:22 +0800

    whale2

--- whale.js
+++ whale.js
@@ -5,43 +5,57 @@ var fs = require('fs');
 var path = require('path');
 ////////////////////////////////////////////////////////////////////////////////
 var cookie = null;
-check('recordFile');
+Array.prototype.unique2 = function() {
+    this.sort(); //先排序
+    var res = [this[0]];
+    for (var i = 1; i < this.length; i++) {
+        if (this[i] !== res[res.length - 1]) {
+            res.push(this[i]);
+        }
+    }
+    return res;
+}
+
+check('whale2');
 
 function check(recordFile, cbfn) {
     cookie = fs.readFileSync(path.join(__dirname, 'cookie.txt'), 'utf-8');
 
     var flag = 0;
     var ids = [];
+    var str = '';
 
-    for (var i = 14; i > 0; --i) {
-        superagent.get('https://www.shanbay.com/team/thread/8854/1840480/?page=' + i + '#p1')
+    for (var i = 17; i > 0; --i) {
+        superagent.get('https://www.shanbay.com/team/thread/8854/2010079/?page=' + i + '#p1')
             .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
             .set('Cookie', cookie)
             .end(function(err, res) {
-                console.log(err, res.text);
-                // return false;
                 var $ = cheerio.load(res.text);
                 ++flag;
 
-                // var ids = [];
                 $('#djangoForumThreadPosts').find('.post').each((idx, post) => {
-                    let teamSrc = $(post).find('.team').find('a').attr('href');
-                    // if (teamSrc != '/team/detail/8854/') return; // 跳过不在小组的
-
+                    let teamSrc = $(post).find('.team').find('a').attr('href') || '';
                     let user = $(post).find('.userinfo > .span3 > a');
                     let content = $(post).find('.post-content-todo').text();
 
-                    // if (content.search('2017青梅鲸鱼') != -1)
-                    if (user.text() != 'KING')
+                    if (user.text() != 'KING' && teamSrc == '/team/detail/8854/' && content.search('2017青梅鲸鱼') != -1)
                         ids.push('[' + user.text() + '](https://www.shanbay.com' + user.attr('href') + ')');
                 });
-                // console.log(JSON.stringify(ids));
 
-                if (flag === 14) {
-                    fs.writeFile(path.join(__dirname, recordFile + '-log.txt'), ids.length + '\n' + JSON.stringify(ids) + '\n', { flag: 'a' }, function(err2) {
+                if (flag === 17) {
+                    fs.writeFile(path.join(__dirname, recordFile + '-log.txt'), ids.length + '\n' + JSON.stringify(ids.unique2()) + '\n', { flag: 'a' }, function(err2) {
                         if (err2) console.log('fs writeFile err: ', err2);
                     });
                     console.log('end: whale');
+
+
+                    ids.forEach(function(e) {
+                        str += ' 1. '+ e + '\n';
+                    }, this);
+
+                    fs.writeFile(path.join(__dirname, 'whale2-list.txt'), '\n' + str + '\n', { flag: 'a' }, function(err2) {
+                        if (err2) console.log('fs writeFile err: ', err2);
+                    });
                 }
             });
     }
