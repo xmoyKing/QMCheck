4931ed9d8c498ffc8775eba7fcab71b490e1b728
xmoyking <xmoyking@gmail.com>
2017-10-05 23:10:58 +0800

    修复时间设置错误

--- routes/cookie.js
+++ routes/cookie.js
@@ -23,7 +23,7 @@ router.post('/upload', function(req, res, next) {
         if (!err) msg = 'succss';
 
         var now = new Date;
-        now.setDate(now.getDate + 9); // 9天后的此时发送提醒邮件
+        now.setDate(now.getDate() + 9); // 9天后的此时发送提醒邮件
         // var date = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), 0);  
 
         var j = schedule.scheduleJob(now, function() {
