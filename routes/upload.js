fd5446af9835c601df1cc7b8540d07d8442a70a9
xmoyking <xmoyking@gmail.com>
2018-05-27 10:40:10 +0800

    upload

--- routes/upload.js
+++ routes/upload.js
@@ -10,7 +10,7 @@ const exec = util.promisify(cp.exec);
 /* GET upload page. */
 router.get('/', function(req, res, next) {
     let totalMsg = '';
-    const cmds = ['git status'] //, 'git add .', 'git commit -m "upload"', 'git push'];
+    const cmds = ['git status', 'git add .', 'git commit -m "upload"', 'git push'];
 
     function renderMsg() {
         // console.log(`\n totalMsg: ${totalMsg} \n`);
@@ -20,15 +20,15 @@ router.get('/', function(req, res, next) {
     }
     async function runCmd(cmds = ['git status'], idx = 0) {
         const {error, stdout, stderr} = await exec(cmds[idx]);
-        console.log('\n cmd: \n', error, stdout, stderr)
+        console.log(`\n cmd: ${cmds[idx]} \n error: \n ${error} \n stdout: \n ${stdout} \n stderr: \n ${stderr}`)
 
         if(error){
             totalMsg += `${error}`;
             renderMsg(); // 当有错误时直接渲染，不执行后面的命令
         }else{
             totalMsg += `${stdout}`;
-            // 执行最后一个命令后渲染页面
-            if(idx == cmds.length - 1){
+            if((idx == 0 && stdout.endsWith('nothing to commit, working tree clean') || (idx == cmds.length - 1))){
+                // 执行最后一个命令后渲染页面
                 renderMsg();
             }else{ // 否则继续继续后续任务
                 runCmd(cmds, idx+1);
