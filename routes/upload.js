6833c7571201b9e974056cf630ef94056484367d
xmoyking <xmoyking@gmail.com>
2018-05-27 10:16:46 +0800

    upload

--- routes/upload.js
+++ routes/upload.js
@@ -7,38 +7,36 @@ const util = require('util');
 const cp = require('child_process');
 const exec = util.promisify(cp.exec);
 
-
 /* GET upload page. */
 router.get('/', function(req, res, next) {
     let totalMsg = '';
+    const cmds = ['git add .', 'git commit -m "upload"', 'git push'];
 
-    async function runCmd(cmd, cb) {
-        let rst = false;
-        const {error, stdout, stderr} = await exec(cmd);
-        console.log('\n', error, stdout, stderr)
+    function renderMsg() {
+        // console.log(`\n totalMsg: ${totalMsg} \n`);
+        res.render('stdout', {
+            message: totalMsg
+        });
+    }
+    async function runCmd(cmds = ['git status'], idx = 0) {
+        const {error, stdout, stderr} = await exec(cmds[idx]);
+        // console.log('\n cmd: \n', error, stdout, stderr)
 
         if(error){
             totalMsg += `${error}`;
+            renderMsg(); // 当有错误时直接渲染，不执行后面的命令
         }else{
             totalMsg += `${stdout}`;
-            rst = true;
+            // 执行最后一个命令后渲染页面
+            if(idx == cmds.length - 1){
+                renderMsg();
+            }else{ // 否则继续继续后续任务
+                runCmd(cmds, idx+1);
+            }
         }
-
-        cb && cb();
-
-        return rst;
-    }
-    function renderMsg() {
-        console.log(`\n totalMsg: ${totalMsg} \n`);
-    
-        res.render('index', {
-            title: totalMsg
-        });
     }
 
-    const cmds = ['git add .', 'git commit -m "upload"', 'git status'];
-
-    runCmd(cmds[0]) && runCmd(cmds[1]) && runCmd(cmds[2], renderMsg);
+    runCmd(cmds);
 });
 
 module.exports = router;
